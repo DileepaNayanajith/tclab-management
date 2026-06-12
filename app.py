f53f683f4507f337a2de6a19179dfb43a8b8f0df
PPS Perera <ppsperera@gmail.com>
2026-06-12 19:24:25 +0530

    fix railway gunicorn deployment

--- app.py
+++ app.py
@@ -1938,7 +1938,5 @@ def delete_media(media_code):
 
 #if __name__ == "__main__":
     #app.run(debug=True, host="127.0.0.1", port=5000)
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
