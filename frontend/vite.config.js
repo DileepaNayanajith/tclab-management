47a5e0d06df8f0ffcefa3aebb2c082fc69a565b8
Dileepa Nayanajith <dileepanayanajith380@gmail.com>
2026-09-12 09:45:34 +0530

    chore: allow Cloudflare demo tunnel host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '127.0.0.1',
-    allowedHosts: ['.loca.lt'],
+    allowedHosts: ['.loca.lt', '.trycloudflare.com'],
     proxy: {
       '/api': 'http://127.0.0.1:8080'
     }
